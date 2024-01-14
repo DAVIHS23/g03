a9c7f4c861eaa54fad92ec76b7f5686e9d408308
Corinne Schwarzentruber <corinne.schwarzentruber@stud.hslu.ch>
2024-01-14 21:01:32 +0100

    use data joins add updateTreemap

--- js/treemap.js
+++ js/treemap.js
@@ -11,6 +11,9 @@ const svgTree = d3.select("#my_treemap")
     .append("g")
     .attr("transform", `translate(${marginTree.left}, ${marginTree.top})`);
 
+// prepare a color scale
+const color = d3.scaleOrdinal(d3.schemeTableau10)
+
 function renameSmallCountries(data) {
     const threshold = 0.005
     routesTotal = d3.sum(data, r => r.routes_count);
@@ -25,9 +28,7 @@ function renameSmallCountries(data) {
     return data
 }
 
-// read the data
-const file2 = "data/df_routes_grouped.csv";
-d3.csv(file2).then(function (data) {
+function updateTreemap(data, selCountry) {
     data=renameSmallCountries(data);
     data = d3.flatRollup(data, D => d3.sum(D, d=>d.routes_count), d => d.continent, d => d.country, d => d.country_name)
     .map(a => ({"continent": a[0], "country": a[1], "routes_count": a[3], "country_name": a[2]}));
@@ -46,28 +47,21 @@ d3.csv(file2).then(function (data) {
         .size([widthTree, heightTree])
         .padding(2)(root);
 
-    // prepare a color scale
-    const color = d3.scaleOrdinal(d3.schemeTableau10)
-
-
     // add the color of the rectangles:
-    let selectedCountry = undefined;
-    let selectedContinent = undefined;
-    svgTree
-    .selectAll("rect")
-    .data(root.leaves())
-    .join("rect")
-        .attr("x", function (d) {return d.x0;})
-        .attr("y", function (d) {return d.y0;})
-        .attr("width", function (d) {return d.x1 - d.x0;})
-        .attr("height", function (d) {return d.y1 - d.y0;})
+    svgTree.selectAll("rect")
+        .data(root.leaves())
+        .join("rect")
+        .attr("x", d => d.x0)
+        .attr("y", d => d.y0)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0)
         .style("stroke", "black")
-        .style("fill-opacity", 0.8)
-        .style("fill", function (d) {return color(d.data.continent);})
+        .style("fill-opacity", d => selCountry === d.data.country ? 1 : 0.8)
+        .style("fill", d => color(d.data.continent))
     // add handler for clicking on country rectangle
     .on("click", function handle(d, i) {
         let country = i.data.country;
-        if (country===selectedCountry) {
+        if (country===selCountry) {
             selectedCountry = undefined;
             selectedCountryName = undefined;
             selectedContinent = undefined;
@@ -77,33 +71,39 @@ d3.csv(file2).then(function (data) {
             selectedContinent = i.data.continent;
         }
         console.log("selected country: " + selectedContinent + "/" + selectedCountry + ", " + selectedCountryName);
-        d3.selectAll("#my_treemap rect").style("fill-opacity", "0.8");
-        if (selectedCountry) {
-            d3.select(this).style("fill-opacity", 1);
-        }
+        // d3.selectAll("#my_treemap rect").style("fill-opacity", "0.8");
+        // if (selectedCountry) {
+        //     d3.select(this).style("fill-opacity", 1);
+        // }
+        updateTreemap(data, selectedCountry);
         updateBarchart(selectedCountry, selectedContinent, selectedCountryName);
     });
 
-    // add the text/label ot the rectangels
-    rects = svgTree.selectAll("text")
+    // add the text/label to the rectangels
+    svgTree.selectAll("text .country")
         .data(root.leaves())
-        .enter();
-    rects.append("text")
+        .join("text")
         .attr("x", function (d) {return d.x0 + 5;}) // +10 to adjust position (more right)
         .attr("y", function (d) {return d.y0 + 15;}) // +20 to adjust position (lower)
         .text(d => d.data.country)
+        .attr("class", "country")
         .attr("font-size", "11px")
         .attr("font-weight", "bold")
         .attr("fill", "white")
-    rects.append("text")
+    svgTree.selectAll("text .count")
+        .data(root.leaves())
+        .join("text")
         .attr("x", function (d) {return d.x0 + 5;}) // +10 to adjust position (more right)
         .attr("y", function (d) {return d.y0 + 30;}) // +20 to adjust position (lower)
         .text(d => d.value)
+        .attr("class", "count")
         .attr("font-size", "11px")
         .attr("fill", "white");
+}
 
-    d3.selectAll("rect")
-
+// read the data
+const file2 = "data/df_routes_grouped.csv";
+d3.csv(file2).then(function (data) {
     //Initialize legend
     const treemapLegendItemSize = 12;
     const treemapLegendSpacing = 4;
@@ -141,8 +141,10 @@ d3.csv(file2).then(function (data) {
         .attr('y', (d, i) => yOffset + 7)
         .text(function(d){ return d});		
 
-        console.log("Treemap: root for Text = ");
-        console.log(continents);
+    console.log("Treemap: root for Text = ");
+    console.log(continents);
 
+    selectedCountry = undefined;
+    updateTreemap(data, selectedCountry);
 });
 
